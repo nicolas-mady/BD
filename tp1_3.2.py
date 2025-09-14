efe4c0d366b2c5fa8f9f8502ba3a773f91ce2833
Nicolas Mady <nicolasmady06@gmail.com>
2025-09-14 11:25:36 -0400

    style: format code for improved readability and consistency

--- tp1_3.2.py
+++ tp1_3.2.py
@@ -5,64 +5,64 @@ import psycopg2 as pg
 
 TXT = "amazon-meta.txt"
 with open("schema.sql") as f:
-	SQL = f.read()
+    SQL = f.read()
 tables = re.findall(r"(\w+) \(\n", SQL)
 PK = {table: set() for table in tables}
 TUPS = {table: [] for table in tables}
 
 
 def nextln(count=1) -> str:
-	return re.sub(r"\w+( \w+)?:", "", next(txt), count=count).strip()
+    return re.sub(r"\w+( \w+)?:", "", next(txt), count=count).strip()
 
 
 def parse_block():
-	next(txt)
-	pid = nextln()
-	asin = nextln()
-	PK["products"].add(asin)
-	title = nextln().replace('"', '""')
+    next(txt)
+    pid = nextln()
+    asin = nextln()
+    PK["products"].add(asin)
+    title = nextln().replace('"', '""')
 
-	if "discontinued" in title:
-		TUPS["products"].append((pid, asin, "", "", "", "", "", "", "", ""))
-		return
+    if "discontinued" in title:
+        TUPS["products"].append((pid, asin, "", "", "", "", "", "", "", ""))
+        return
 
-	grp = nextln()
-	srank = nextln()
-	sims, *asins = nextln().split()
+    grp = nextln()
+    srank = nextln()
+    sims, *asins = nextln().split()
 
-	for sim in asins:
-		TUPS["similars"].append((asin, sim))
+    for sim in asins:
+        TUPS["similars"].append((asin, sim))
 
-	cats = nextln()
+    cats = nextln()
 
-	for _ in range(int(cats)):
-		super_id = ""
-		for descr, cid in re.findall(r"\|([^\[]*)\[(\d+)\]", next(txt)):
-			if cid not in PK["categories"]:
-				PK["categories"].add(cid)
-				TUPS["categories"].append((cid, descr, super_id))
-			if (asin, cid) not in PK["products_categories"]:
-				PK["products_categories"].add((asin, cid))
-				TUPS["products_categories"].append((asin, cid))
-			super_id = cid
+    for _ in range(int(cats)):
+        super_id = ""
+        for descr, cid in re.findall(r"\|([^\[]*)\[(\d+)\]", next(txt)):
+            if cid not in PK["categories"]:
+                PK["categories"].add(cid)
+                TUPS["categories"].append((cid, descr, super_id))
+            if (asin, cid) not in PK["products_categories"]:
+                PK["products_categories"].add((asin, cid))
+                TUPS["products_categories"].append((asin, cid))
+            super_id = cid
 
-	rev = nextln(0).split()
+    rev = nextln(0).split()
 
-	TUPS["products"].append((pid, asin, title, grp, srank, sims, cats, *rev))
+    TUPS["products"].append((pid, asin, title, grp, srank, sims, cats, *rev))
 
-	for _ in range(int(rev[1])):
-		rid = len(TUPS["reviews"])
-		TUPS["reviews"].append((rid, asin, *nextln(0).split()))
+    for _ in range(int(rev[1])):
+        rid = len(TUPS["reviews"])
+        TUPS["reviews"].append((rid, asin, *nextln(0).split()))
 
 
 with open(TXT) as txt:
-	try:
-		next(txt)
-		next(txt)
-		while True:
-			parse_block()
-	except StopIteration:
-		pass
+    try:
+        next(txt)
+        next(txt)
+        while True:
+            parse_block()
+    except StopIteration:
+        pass
 
 TUPS["similars"] = [t for t in TUPS["similars"] if t[1] in PK["products"]]
 
@@ -70,18 +70,18 @@ os.system("dropdb ecommerce")
 os.system("createdb ecommerce")
 
 with pg.connect(
-	dbname="ecommerce",
-	user="postgres",
-	password="postgres",
-	host="localhost",
-	port="5432"
+        dbname="ecommerce",
+        user="postgres",
+        password="postgres",
+        host="localhost",
+        port="5432"
 ) as conn:
-	with conn.cursor() as curs:
-		curs.execute(SQL)
-		for table, tups in TUPS.items():
-			with open(table, "w") as f:
-				csv.writer(f).writerows(tups)
-			with open(table) as f:
-				curs.copy_expert(f"COPY {table} FROM STDIN WITH CSV", f)
-			os.remove(table)
-	conn.commit()
+    with conn.cursor() as curs:
+        curs.execute(SQL)
+        for table, tups in TUPS.items():
+            with open(table, "w") as f:
+                csv.writer(f).writerows(tups)
+            with open(table) as f:
+                curs.copy_expert(f"COPY {table} FROM STDIN WITH CSV", f)
+            os.remove(table)
+    conn.commit()
