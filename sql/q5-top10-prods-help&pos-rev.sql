3b630e548646570a04082ce11e8d7593a9a88b9a
Nicolas Mady <nicolasmady06@gmail.com>
2025-09-24 12:13:47 -0400

    fix: update SQL query to calculate average helpfulness and improve filtering criteria for top products

--- sql/q5-top10-prods-help&pos-rev.sql
+++ sql/q5-top10-prods-help&pos-rev.sql
@@ -1,38 +1,11 @@
 /* 5. Listar os 10 produtos com a maior média de avaliações úteis positivas por produto.
  */
--- SELECT p.pasin,
---        p.title,
---        ROUND(AVG(r.helpful), 2) AS avg_helpful
--- FROM products p
--- JOIN reviews r ON p.pasin = r.pasin
--- GROUP BY p.pasin, p.title
--- ORDER BY avg_helpful DESC
--- LIMIT 10;
-
 SELECT p.pasin,
        p.title,
-       AVG(r.rating) AS avg_rating,
-       AVG(CAST(r.helpful AS FLOAT) / NULLIF(r.votes, 0)) AS avg_helpful_ratio,
-       COUNT(r.pasin) AS total_reviews,
-       COUNT(CASE
-                 WHEN r.rating > 3 THEN 1
-             END) AS positive_reviews
+       ROUND(AVG(r.helpful), 2) AS avg_helpful_positive
 FROM products p
 JOIN reviews r ON p.pasin = r.pasin
-WHERE
-    r.votes > 0
-    AND r.rating > 3
-GROUP BY p.pasin, p.title, p.grp
--- HAVING COUNT(r.pasin) >= 3 -- Pelo menos 3 reviews para ser considerado
-ORDER BY
-    AVG(CAST(r.helpful AS FLOAT) / NULLIF(r.votes, 0)) DESC,
-    AVG(r.rating) DESC
+WHERE r.rating >= 4 AND r.helpful > 0
+GROUP BY p.pasin, p.title
+ORDER BY avg_helpful_positive DESC
 LIMIT 10;
-
--- SELECT pasin, title
--- FROM reviews
--- NATURAL JOIN products
--- WHERE votes > 0 AND rating > 3
--- GROUP BY pasin, title, helpful, votes
--- ORDER BY helpful / votes DESC, AVG(rating) DESC
--- LIMIT 10;
